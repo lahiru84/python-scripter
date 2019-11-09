41e67273e228304dd5c17084b75f0a29a1460a37
Nimesh <lahiru.jayasundara@gmail.com>
2019-11-09 21:12:05 +0530

    Setup script

---
+++ nodejs-vision-master/samples/faceDetection.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2016, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START vision_face_detection_tutorial_imports]
+// By default, the client will authenticate using the service account file
+// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
+// the project specified by the GCLOUD_PROJECT environment variable. See
+// https://googlecloudplatform.github.io/gcloud-node/#/docs/google-cloud/latest/guides/authentication
+const vision = require('@google-cloud/vision');
+// [END vision_face_detection_tutorial_imports]
+// [START vision_face_detection_tutorial_client]
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+const fs = require('fs');
+// [END vision_face_detection_tutorial_client]
+
+/**
+ * Uses the Vision API to detect faces in the given file.
+ */
+// [START vision_face_detection_tutorial_send_request]
+async function detectFaces(inputFile) {
+  // Make a call to the Vision API to detect the faces
+  const request = {image: {source: {filename: inputFile}}};
+  const results = await client.faceDetection(request);
+  const faces = results[0].faceAnnotations;
+  const numFaces = faces.length;
+  console.log(`Found ${numFaces} face${numFaces === 1 ? '' : 's'}.`);
+  return faces;
+}
+// [END vision_face_detection_tutorial_send_request]
+
+/**
+ * Draws a polygon around the faces, then saves to outputFile.
+ */
+// [START vision_face_detection_tutorial_process_response]
+async function highlightFaces(inputFile, faces, outputFile, Canvas) {
+  const {promisify} = require('util');
+  const readFile = promisify(fs.readFile);
+  const image = await readFile(inputFile);
+  const Image = Canvas.Image;
+  // Open the original image into a canvas
+  const img = new Image();
+  img.src = image;
+  const canvas = new Canvas.Canvas(img.width, img.height);
+  const context = canvas.getContext('2d');
+  context.drawImage(img, 0, 0, img.width, img.height);
+
+  // Now draw boxes around all the faces
+  context.strokeStyle = 'rgba(0,255,0,0.8)';
+  context.lineWidth = '5';
+
+  faces.forEach(face => {
+    context.beginPath();
+    let origX = 0;
+    let origY = 0;
+    face.boundingPoly.vertices.forEach((bounds, i) => {
+      if (i === 0) {
+        origX = bounds.x;
+        origY = bounds.y;
+      }
+      context.lineTo(bounds.x, bounds.y);
+    });
+    context.lineTo(origX, origY);
+    context.stroke();
+  });
+
+  // Write the result to a file
+  console.log(`Writing to file ${outputFile}`);
+  const writeStream = fs.createWriteStream(outputFile);
+  const pngStream = canvas.pngStream();
+
+  await new Promise((resolve, reject) => {
+    pngStream
+      .on('data', chunk => writeStream.write(chunk))
+      .on('error', reject)
+      .on('end', resolve);
+  });
+}
+// [END vision_face_detection_tutorial_process_response]
+
+// Run the example
+// [START vision_face_detection_tutorial_run_application]
+async function main(inputFile, outputFile) {
+  const Canvas = require('canvas');
+  outputFile = outputFile || 'out.png';
+  const faces = await detectFaces(inputFile);
+  console.log('Highlighting...');
+  await highlightFaces(inputFile, faces, outputFile, Canvas);
+  console.log('Finished!');
+}
+// [END vision_face_detection_tutorial_run_application]
+
+const args = process.argv.slice(2);
+main(...args).catch(console.error);
---
+++ samples/README.md
@@ -0,0 +1,134 @@
+[//]: # "This README.md file is auto-generated, all changes to this file will be lost."
+[//]: # "To regenerate it, use `python -m synthtool`."
+<img src="https://avatars2.githubusercontent.com/u/2810941?v=3&s=96" alt="Google Cloud Platform logo" title="Google Cloud Platform" align="right" height="96" width="96"/>
+
+# [Google Cloud Vision API: Node.js Samples](https://github.com/googleapis/nodejs-vision)
+
+[![Open in Cloud Shell][shell_img]][shell_link]
+
+
+
+## Table of Contents
+
+* [Before you begin](#before-you-begin)
+* [Samples](#samples)
+  * [Detect](#detect)
+  * [Detect.v1p1beta1](#detect.v1p1beta1)
+  * [Detect.v1p3beta1](#detect.v1p3beta1)
+  * [Face Detection](#face-detection)
+  * [Quickstart](#quickstart)
+  * [Text Detection](#text-detection)
+
+## Before you begin
+
+Before running the samples, make sure you've followed the steps outlined in
+[Using the client library](https://github.com/googleapis/nodejs-vision#using-the-client-library).
+
+## Samples
+
+
+
+### Detect
+
+View the [source code](https://github.com/googleapis/nodejs-vision/blob/master/samples/detect.js).
+
+[![Open in Cloud Shell][shell_img]](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/googleapis/nodejs-vision&page=editor&open_in_editor=samples/detect.js,samples/README.md)
+
+__Usage:__
+
+
+`node detect.js`
+
+
+-----
+
+
+
+
+### Detect.v1p1beta1
+
+View the [source code](https://github.com/googleapis/nodejs-vision/blob/master/samples/detect.v1p1beta1.js).
+
+[![Open in Cloud Shell][shell_img]](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/googleapis/nodejs-vision&page=editor&open_in_editor=samples/detect.v1p1beta1.js,samples/README.md)
+
+__Usage:__
+
+
+`node detect.v1p1beta1.js`
+
+
+-----
+
+
+
+
+### Detect.v1p3beta1
+
+View the [source code](https://github.com/googleapis/nodejs-vision/blob/master/samples/detect.v1p3beta1.js).
+
+[![Open in Cloud Shell][shell_img]](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/googleapis/nodejs-vision&page=editor&open_in_editor=samples/detect.v1p3beta1.js,samples/README.md)
+
+__Usage:__
+
+
+`node detect.v1p3beta1.js`
+
+
+-----
+
+
+
+
+### Face Detection
+
+View the [source code](https://github.com/googleapis/nodejs-vision/blob/master/samples/faceDetection.js).
+
+[![Open in Cloud Shell][shell_img]](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/googleapis/nodejs-vision&page=editor&open_in_editor=samples/faceDetection.js,samples/README.md)
+
+__Usage:__
+
+
+`node faceDetection.js`
+
+
+-----
+
+
+
+
+### Quickstart
+
+View the [source code](https://github.com/googleapis/nodejs-vision/blob/master/samples/quickstart.js).
+
+[![Open in Cloud Shell][shell_img]](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/googleapis/nodejs-vision&page=editor&open_in_editor=samples/quickstart.js,samples/README.md)
+
+__Usage:__
+
+
+`node quickstart.js`
+
+
+-----
+
+
+
+
+### Text Detection
+
+View the [source code](https://github.com/googleapis/nodejs-vision/blob/master/samples/textDetection.js).
+
+[![Open in Cloud Shell][shell_img]](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/googleapis/nodejs-vision&page=editor&open_in_editor=samples/textDetection.js,samples/README.md)
+
+__Usage:__
+
+
+`node textDetection.js`
+
+
+
+
+
+
+[shell_img]: https://gstatic.com/cloudssh/images/open-btn.png
+[shell_link]: https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/googleapis/nodejs-vision&page=editor&open_in_editor=samples/README.md
+[product-docs]: https://cloud.google.com/vision
